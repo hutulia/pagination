11b1ab1b60e4287612aad25b5137cf6b651b74e0
Vitalii Bondarenko <bovile17051992@gmail.com>
2022-10-04 18:34:57 +0300

    Add some tests

--- tests/PaginationTest.php
+++ tests/PaginationTest.php
@@ -81,5 +81,23 @@ class PaginationTest extends TestCase
         //< {TOTAL}|{PER_PAGE}|{TOTAL_PAGES}|{CURRENT_PAGE}|{IS_START_PAGE}|{IS_END_PAGE}|{TOTAL_ON_CURRENT_PAGE}|{START}|{END}
         //var_dump($fixture);
         $this->assertSame('10|3|4|4|0|1|1|10|10', $fixture->render($template));
+
+        $fixture = new Pagination(12, 5, 2);
+        $renderVars = $fixture->getRenderVars();
+        $varNames = array_keys($renderVars);
+        $varsAsTemplateString = array_map(function($varName){
+            return '{'.$varName.'}';
+        }, $varNames);
+        $template = implode('|', $varsAsTemplateString);
+        $this->assertSame('12|5|3|2|0|0|5|6|10', $fixture->render($template));
+
+        $fixture = new Pagination(12, 5, 3);
+        $renderVars = $fixture->getRenderVars();
+        $varNames = array_keys($renderVars);
+        $varsAsTemplateString = array_map(function($varName){
+            return '{'.$varName.'}';
+        }, $varNames);
+        $template = implode('|', $varsAsTemplateString);
+        $this->assertSame('12|5|3|3|0|1|2|11|12', $fixture->render($template));
     }
 }
